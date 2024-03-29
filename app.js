62e6fa75ea13cf119914862f8fb939f6c1d8de39
Ajaybabulalkore <146156497+Ajaybabulalkore@users.noreply.github.com>
2024-03-29 17:08:08 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+/* ==============================================  typing animation =================================================  */
+
+var typed = new Typed(".typing", {
+    strings:["Frontend devloper" , "photographer"],
+    typeSpeed: 100,
+    BackSpeed: 40,
+    loop:true
+})
